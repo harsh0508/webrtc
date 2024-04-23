fff541dec379041bcf993b6234cefd2a3e9eff51
Harsh raj <Harsh.jvm@gmail.com>
2024-04-23 12:56:49 +0530

    new

--- src/firebase.js
+++ src/firebase.js
@@ -8,13 +8,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBamlM-2WwCWWMs2HDtENkU3G_4SM5sDM4",
-  authDomain: "webrtc-8124d.firebaseapp.com",
-  projectId: "webrtc-8124d",
-  storageBucket: "webrtc-8124d.appspot.com",
-  messagingSenderId: "182734991063",
-  appId: "1:182734991063:web:d7e5a7ccd4f2f6f5860a92",
-  measurementId: "G-KEH913GD4Y"
+  // your config
 };
 
 // Initialize Firebase
